aa3573cc92781b83bb92396cba85a159af8167fe
Eric Halverson <ericleifhalverson@gmail.com>
2023-01-07 16:47:25 -0800

    Add basic error handling for user response

--- daily_horoscope.py
+++ daily_horoscope.py
@@ -1,12 +1,10 @@
-#{"date_range": "Jan 20 - Feb 18", "current_date": "January 7, 2023", "description": "It doesn't take much persuasion on your part to get a certain someone to see your side of things -- a few well-placed words will take care of that. In fact, your charm is so powerful, it's almost unfair.", "compatibility": "Leo", "mood": "Charming", "color": "Copper", "lucky_number": "42", "lucky_time": "2am"}
-
 import requests
 import json
 
 
 class Horoscope:
     def __init__(self, sign):
-        self.sign = sign
+        self.sign = sign.lower()
         self.astro_data = {} 
     def get_horoscope(self):
         url = "https://sameer-kumar-aztro-v1.p.rapidapi.com/"
@@ -20,11 +18,23 @@ class Horoscope:
 
 
 response = input("Hello! Would you like your horoscope for today? (y,n) ")
-if response == 'y':
-    sign = input("Great! What is your sign? ")
-    player = Horoscope(sign)
-    player.get_horoscope()
-    print()
-    print(player.astro_data["description"])
-else:
+if response not in ['yes', 'Yes', 'y', 'Y']:
     print("Ok. Maybe another time.")
+else:
+    sign = input("Great! What is your sign? ")
+    appropriate_signs = ["aries", "taurus", "gemini", "cancer", "leo", "virgo", "libra", "scorpio", "sagittarius", "capricorn",
+                         "aquarius", "pisces"]
+    if sign.lower() in appropriate_signs:
+        player = Horoscope(sign)
+        player.get_horoscope()
+        print()
+        print("For {}:".format(player.astro_data["current_date"]))
+        print("Info: {}".format(player.astro_data["description"]))
+        print("Compatibility: {}".format(player.astro_data["compatibility"]))
+        print("Mood: {}".format(player.astro_data["mood"]))
+        print("Color: {}".format(player.astro_data["color"]))
+        print("Lucky Number: {}".format(player.astro_data["lucky_number"]))
+        print("Lucky Time: {}".format(player.astro_data["lucky_time"]))
+    else:
+        print("Sorry. You may have a typo.")
+        
